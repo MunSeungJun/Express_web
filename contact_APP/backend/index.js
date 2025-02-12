3fb17bf5b41b57a78c2a1288c5d8d407f7fe43c0
seungjun <sbs2469@naver.com>
2025-02-12 10:21:34 +0900

    서버 | 라우팅 처리 삭제

--- contact_APP/backend/index.js
+++ contact_APP/backend/index.js
@@ -7,10 +7,11 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
-// app.get('/contacts', (req,res)=>{
-//     res.status(200).json(user)
-// })
-app.post('/add', (req, res) => {
+/* app.get('/contacts', (req,res)=>{
+    res.status(200).json(user)
+}) */
+
+/* app.post('/add', (req, res) => {
     const {input_name, phone, job, memo} = req.body
     user = {
         name: input_name,
@@ -22,7 +23,7 @@ app.post('/add', (req, res) => {
         status:200
     })
     console.log(user)
-})
+}) */
 
 app.listen(port, () => {
     console.log(`listen ${port}`)
