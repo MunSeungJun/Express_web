eae588abc878bfe997acafea735d3f1004f4637c
seungjun <sbs2469@naver.com>
2025-02-12 10:19:32 +0900

    연락처 등록 | 포스트 요청 수정

--- contact_APP/frontend/create.js
+++ contact_APP/frontend/create.js
@@ -3,20 +3,22 @@ const server_url = "http://localhost:3000/contacts"
 
 createForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    const inputName = createForm.querySelector("#input_name").value
+    const name = createForm.querySelector("#input_name").value
     const phone = createForm.querySelector("#phone").value
     const job = createForm.querySelector("#job").value
     const memo = createForm.querySelector("#memo").value
 
     let inputData = {
-        name: inputName,
+        name: name,
         phone: phone,
         job: job,
         memo: memo
     }
-    postData(server_url, inputData).then((data) => {
-        console.log(data)
-    })
+    // postData(server_url, inputData).then((data) => {
+    //     console.log(data)
+    // })
+
+    postData(server_url, inputData)
 })
 async function postData(url = "", data = {}) {
     const response = await fetch(url, {
