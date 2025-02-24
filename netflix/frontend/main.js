8e2d64e2a60a7157a6218dbaace5afac575aba2b
seungjun <sbs2469@naver.com>
2025-02-24 10:55:32 +0900

    netflix | carousel 완성

--- netflix/frontend/main.js
+++ netflix/frontend/main.js
@@ -1,23 +1,19 @@
 const prevBtn = document.querySelector("#prev_btn")
 const nextBtn = document.querySelector("#next_btn")
 const carouselList = document.querySelector(".carousel_list")
-let count = 0
 
 nextBtn.addEventListener("click", ()=>{
-    if(count == 0) {
         prevBtn.classList.remove("d-none")
-        count++
-        carouselList.style.left = "-774px"
-    } else {
-        count++
-        carouselList.style.left = "-1395px"
-    }
+        nextBtn.classList.add("d-none")
+        carouselList.style.left = "-1290px"
     
 })
 
 
 
 prevBtn.addEventListener("click",()=>{
-    // if( carouselList.style)
+    prevBtn.classList.add("d-none")
+    nextBtn.classList.remove("d-none")
+    carouselList.style.left = "0px"
     
 })
